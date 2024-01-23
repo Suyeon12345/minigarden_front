2bc02298f5c3915db1517963192d91b89b91cf52
suyeon <antndus2360@gmail.com>
2024-01-23 16:28:19 +0900

    slacktest_suyeon_240123

--- src/flower/component/List.jsx
+++ src/flower/component/List.jsx
@@ -50,7 +50,7 @@ const DeptList = () => {
                 type="text"
                 id="keyword"
                 className="form-control"
-                placeholder="검색어를 입력하세요"
+                placeholder="검색어를 입력하세요!"
                 aria-label="검색어를 입력하세요"
                 aria-describedby="btn_search"
               />
