b8c972639c7a6f1fdf46bf1213ea1c77bd806edd
suyeon <antndus2360@gmail.com>
2024-01-24 20:13:05 +0900

    유효성검사 추가_suyeon_240124

--- src/flower/component/UpdateModal.jsx
+++ src/flower/component/UpdateModal.jsx
@@ -1,54 +1,41 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
 import {DeptUpdateDB} from "../service/dbLogic";
 import { Row, Col } from "antd";
 import {useSelector} from "react-redux";
+import InputNumber from "./InputNumber";
+import { useForm } from "react-hook-form";
 
 const UpdateModal = ({ handleRefresh }) => {
     const dno = useSelector((state) => state.detailInfo.value);
+
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm({ mode: "onChange" });
+
+    const onSubmit = async (values) => {
+        console.log(values.id)
+        console.log(values.name)
+        console.log(values.nae)
+    const data = {
+            d_no : dno.D_NO,
+            d_id : values.id,
+             d_name : values.name,
+             d_nae : values.nae
+    }
+        const res = await DeptUpdateDB(data);
+        alert("게시글이 수정되었습니다!!");
+        handleClose();
+        handleRefresh()
+    };
 
     ///모달
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    //////등록
-    const [id, setId] = useState("");
-    const [name, setName] = useState("");
-    const [nae, setNae] = useState("");
-    ///
-
-    ///부서 등록 입력 값 저장
-    const handleid = useCallback((value) => {
-        setId(value);
-    }, []);
-
-    const handlename = useCallback((value) => {
-        setName(value);
-    }, []);
-
-    const handlenae = useCallback((value) => {
-        setNae(value);
-    }, []);
-
-    ///deptinsert
-    const deptUpdate = async () => {
-        const dept = {
-            d_no : dno.D_NO,
-            d_id: id,
-            d_name: name,
-            d_nae: nae,
-        };
-        console.log("update =" + dept + dno.D_NO + id + name + nae)
-        if (id == "" || name == "" || nae == "") {
-            alert("항목 값을 입력해주세요!!");
-        } else {
-            const res = await DeptUpdateDB(dept);
-            alert("게시글이 수정되었습니다!!");
-            handleClose();
-            handleRefresh()
-        }
-    };
 
     return (
         <>
@@ -59,24 +46,27 @@ const UpdateModal = ({ handleRefresh }) => {
                 <Modal.Header closeButton>
                     <Modal.Title>부서수정</Modal.Title>
                 </Modal.Header>
+                <Form onSubmit={handleSubmit(onSubmit)}>
                 <Modal.Body>
-                    <Form>
+
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>부서코드</Form.Label>
                             <Row>
                                 <Col>
-                                    <Form.Control
+                                    <InputNumber
+                                        {...register("id", {required: "부서코드를 입력해주세요."})}
                                         type="text"
                                         autoFocus
                                         defaultValue={dno.D_ID}
-                                        //onchange -> 변경감지 -> 입력값 handleid에 넘김
-                                        onChange={(e) => {
-                                            handleid(e.target.value);
-                                        }}
+                                        maxLength={10}
+                                        thousandSeparator=","
+                                        allowNegative={false}
                                     />
+                                    <p>{errors?.id?.message}</p>
                                 </Col>
+
                                 <Col>
-                                    <Button className="ms-3">중복검사</Button>
+                                <Button className="ms-3">중복검사</Button>
                                 </Col>
                             </Row>
                         </Form.Group>
@@ -85,50 +75,50 @@ const UpdateModal = ({ handleRefresh }) => {
                             <Row>
                                 <Col>
                                     <Form.Control
+                                        {...register("name", {required: "부서명을 입력해주세요."})}
                                         type="text"
                                         autoFocus
                                         defaultValue={dno.D_NAME}
-                                        //onchange -> 변경감지 -> 입력값 handleid에 넘김
-                                        onChange={(e) => {
-                                            handlename(e.target.value);
-                                        }}
                                     />
+                                    <p>{errors?.name?.message}</p>
                                 </Col>
                                 <Col>
-                                    <Button className="ms-3">중복검사</Button>
+                                <Button className="ms-3">중복검사</Button>
                                 </Col>
                             </Row>
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Group className="d_name" controlId="exampleForm.ControlInput1">
                             <Form.Label>내선번호</Form.Label>
                             <Row>
                                 <Col>
-                                    <Form.Control
+                                    <InputNumber
+                                        {...register("nae", {required: "내선번호를 입력해주세요."})}
                                         type="text"
                                         autoFocus
                                         defaultValue={dno.D_NAE}
-                                        //onchange -> 변경감지 -> 입력값 handleid에 넘김
-                                        onChange={(e) => {
-                                            handlenae(e.target.value);
-                                        }}
+                                        maxLength={10}
+                                        thousandSeparator=","
+                                        allowNegative={false}
                                     />
+                                    <p>{errors?.nae?.message}</p>
                                 </Col>
                                 <Col>
                                     <Button className="ms-3">중복검사</Button>
                                 </Col>
                             </Row>
                         </Form.Group>
-                    </Form>
+
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         취소
                     </Button>
-                    <Button variant="primary" onClick={deptUpdate}>
+                    <Button variant="primary" type = "submit">
                         수정
                     </Button>
                 </Modal.Footer>
-            </Modal>
+                </Form>
+                </Modal>
         </>
     );
 };
